70cbfb0bf4e0ce5cfdddefd31a18d13284b8874b
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-05-09 17:01:22 +0200

    monet: Go away Megvii
    * Nuke Megvii proprietary files and build stub ones from hardware/xiaomi
    
    Change-Id: I53f8f0897b25517741a06c5962c02fe8fda61072

--- device.mk
+++ device.mk
@@ -35,6 +35,11 @@ TARGET_SCREEN_WIDTH := 1080
 
 PRODUCT_SHIPPING_API_LEVEL := 29
 
+# Camera
+PRODUCT_PACKAGES += \
+    libMegviiFacepp-0.5.2 \
+    libmegface
+
 # Fingerprint
 TARGET_HAS_UDFPS := true
 
