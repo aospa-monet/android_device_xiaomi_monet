6fe251cd7b901d37d009c86a85fd618388701eaa
Alec Chan <88945170+alecchangod@users.noreply.github.com>
2022-12-04 13:25:41 +0800

    monet: trying to fix maintainer name (1/3)

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,3 +19,6 @@ TARGET_KERNEL_CONFIG := monet_defconfig
 
 # Inherit from the proprietary version
 include vendor/xiaomi/monet/BoardConfigVendor.mk
+
+# Properties
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
