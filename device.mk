8c65a50479454fd2c250927b6db263881c541bc7
Alec Chan <alec3d02@gmail.com>
2022-12-14 15:33:25 +0800

    monet: overlay: Merge overlay-xtended into overlay

--- device.mk
+++ device.mk
@@ -13,8 +13,7 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-xtended
+    $(LOCAL_PATH)/overlay
 
 # Board
 PRODUCT_USES_QCOM_HARDWARE := true
