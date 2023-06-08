9dea0030109f924de7a11e9a11d967e48b8d71ea
Alec Chan <88945170+alecchangod@users.noreply.github.com>
2023-06-08 15:34:43 +0800

    monet: Nuke miui camera for now

--- xtended_monet.mk
+++ xtended_monet.mk
@@ -18,7 +18,7 @@ $(call inherit-product, device/xiaomi/monet/device.mk)
 $(call inherit-product, vendor/xiaomi-firmware/monet/firmware.mk)
 
 # MIUI Camera
-$(call inherit-product, vendor/xiaomi/monet-miuicamera/products/miuicamera.mk)
+# $(call inherit-product, vendor/xiaomi/monet-miuicamera/products/miuicamera.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_NAME := xtended_monet
