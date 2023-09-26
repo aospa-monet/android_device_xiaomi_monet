0eb3bfb80091de98a50aa0f4f993c077613db8fd
Alec Chan <alec3d02@gmail.com>
2023-09-26 23:05:58 +0800

    monet: initial lineage

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-   $(LOCAL_DIR)/xtended_monet.mk
+   $(LOCAL_DIR)/lineage_monet.mk
 
 COMMON_LUNCH_CHOICES := \
-    xtended_monet-user \
-    xtended_monet-userdebug \
-    xtended_monet-eng
+    lineage_monet-user \
+    lineage_monet-userdebug \
+    lineage_monet-eng
--- lineage_monet.mk
+++ lineage_monet.mk
@@ -8,8 +8,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Xtended stuff.
-$(call inherit-product, vendor/xtended/config/common_full_phone.mk)
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from monet device
 $(call inherit-product, device/xiaomi/monet/device.mk)
@@ -18,10 +18,10 @@ $(call inherit-product, device/xiaomi/monet/device.mk)
 $(call inherit-product, vendor/xiaomi-firmware/monet/firmware.mk)
 
 # MIUI Camera
-$(call inherit-product, vendor/xiaomi/monet-miuicamera/products/miuicamera.mk)
+# $(call inherit-product, vendor/xiaomi/monet-miuicamera/products/miuicamera.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_NAME := xtended_monet
+PRODUCT_NAME := lineage_monet
 PRODUCT_DEVICE := monet
 BOARD_VENDOR := Xiaomi
 TARGET_VENDOR := Xiaomi
@@ -32,16 +32,6 @@ PRODUCT_MODEL := M2002J9G
 TARGET_INCLUDE_PIXEL_CHARGER := true
 
 
-# Xtended Stuffs
-XTENDED_BUILD_MAINTAINER := alecchangod
-XTENDED_BUILD_TYPE := OFFICIAL
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_SUPPORTS_QUICK_TAP := true
-XTENDED_BUILD_DONATE_URL := https://www.paypal.com/paypalme/Alec174
-
-# Build fingerprint
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="cheetah-user 13 TQ3A.230605.012 10204971 release-keys"
-
-BUILD_FINGERPRINT := google/cheetah/cheetah:13/TQ3A.230605.012/10204971:user/release-keys
