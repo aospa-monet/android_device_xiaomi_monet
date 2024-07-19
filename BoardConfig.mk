edc4621c727fe9282e8c59f8406d49e9085508ed
Alec Chan <alecchangod@gmail.com>
2024-07-20 01:23:50 +0800

    monet: commonize kernel config

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,9 +18,6 @@ TARGET_SCREEN_DENSITY := 440
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):init_xiaomi_monet
 TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_monet
 
-# Kernel
-TARGET_KERNEL_CONFIG := monet_defconfig
-
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
